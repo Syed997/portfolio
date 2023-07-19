57e5847b41fa69cbc20e489f00df19cb210ae853
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-07-19 18:29:22 +0600

    finishing touch

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,7 @@ function Contact() {
                     className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none my-4"
                     />
                     <textarea name="message" placeholder='Enter your message' rows="10" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"></textarea>
-                    <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Submit</button>  
+                    <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 border-2">Submit</button>  
                 </form>
             </div>
         </div>
